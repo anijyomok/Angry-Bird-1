67d7c22d10a760f22cb04101b9753228b6502efd
anijyomok <64972553+anijyomok@users.noreply.github.com>
2020-06-04 13:41:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,41 +3,59 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1,box2,ground;
+var box1, pig1;
+var backgroundImage;
+function preload(){
+ backgroundImage = loadImage("sprites//bg.png");
 
+}
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-    box2 = new Box(700,320,70,70);
-    box1 = new Box(920,320,70,70);
-    Pig1 = new Pig(810,350);
-    Log1 = new Log(810,260,300,PI/2);
+
+    
+    ground = new Ground(600,height,1200,20)
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    Pig2 = new Pig(810,220);
-    Log2 = new Log(810,180,300,PI/2);
-    Log3 = new Log(760,120,150,PI/7);
-    Log4 = new Log(870,120,150,-PI/7);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
     box5 = new Box(810,160,70,70);
-    ground = new Ground (600,390,1200,10);
-    bird = new Bird(200,100);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
 }
 
 function draw(){
-    background(0);
+    background(backgroundImage);
     Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
     box1.display();
-   box2.display();
-   Pig1.display();
-   Log1.display();
-   box3.display();
-   box4.display();
-   Pig2.display();
-   Log2.display();
-   box5.display();
-   Log3.display();
-   Log4.display();
-   ground.display();
-   bird.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
 }
